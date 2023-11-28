b28d99949fda75d6342ceb633ee3666c73ed7f45
Bruno Jaciel Mello <bruno_mello@edu.univali.br>
2023-11-28 14:54:36 -0300

    Interrupt implementation

--- main/main.c
+++ main/main.c
@@ -2,26 +2,30 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/touch_pad.h"
+#include "freertos/queue.h"
+#include "soc/rtc_periph.h"
+#include "soc/sens_periph.h"
 #include "esp_log.h"
 #include "freertos/semphr.h"
 
 #define TOUCH_THRESH_NO_USE   (0)
+#define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
 
-bool car_system_sensors[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-bool car_system_actuator[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-//0: electronic injection | 1: internal temperature | 
-//2: ABS right whell | 3: ABS left whell |
-//4: airbag | 5: seat belt | 
+static const char *TAG = "Touch pad";
 
-//6: front headlight light right | 7: front headlight light left | 
-//8: power window system right | 9: power window system left
-//10: two door lock right | 11: two door lock left 
+static bool s_pad_activated[TOUCH_PAD_MAX];
+static uint32_t s_pad_init_val[TOUCH_PAD_MAX];
+
+bool car_system_actuator[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//0: electronic injection | 3: internal temperature | 
+//4: ABS | 5: airbag | 6: seat belt | 
+//7: front headlight light | 8: power window system | 9: two door lock
 
 SemaphoreHandle_t mutex; // Declare a mutex handle
 
 void read_sensor(int index){
     xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-    car_system_sensors[index] = !car_system_sensors[index];
+    s_pad_activated[index] = false;
     xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
 }
 
@@ -32,19 +36,22 @@ void execute_actuator(int index){
 }
 
 void thread_display(void) {
-    printf("\nENGINE: Actuator electronic injection: %d | Actuator internal temperature: %d\n", car_system_actuator[0], car_system_actuator[1]);
-    printf("BRAKE: ABS right: %d | ABS left: %d\n", car_system_actuator[2], car_system_actuator[3]);
-    printf("LSE: Airbag: %d | Seat belt: %d\n", car_system_actuator[4], car_system_actuator[5]);
-    printf("LVT: FHL right: %d | FHL left: %d | PWS right: %d | PWS left: %d | TDL right: %d | TDL left: %d\n", car_system_actuator[6], car_system_actuator[7], car_system_actuator[8], car_system_actuator[9], car_system_actuator[10], car_system_actuator[11]);
-        
+    printf("\033[2J\033[1;1H");
+    printf("\nENGINE: Actuator electronic injection: %d | Actuator internal temperature: %d\n", car_system_actuator[0], car_system_actuator[3]);
+    printf("BRAKE: ABS %d\n", car_system_actuator[4]);
+    printf("LSE: Airbag: %d | Seat belt: %d\n", car_system_actuator[5], car_system_actuator[6]);
+    printf("LVT: Front Headlight Light: %d | Power Window System: %d | Two Door Lock: %d\n", car_system_actuator[7], car_system_actuator[8], car_system_actuator[9]);
+
     vTaskDelay(500/portTICK_PERIOD_MS);
 }
 
 static void tp_execute_task(void *pvParameter){
     while(1)
     {
+        //interrupt mode, enable touch interrupt
+        touch_pad_intr_enable();
         for (int i = 0; i < 12; i++) {
-            if (car_system_sensors[i] == 1) {
+            if (s_pad_activated[i] == 1) {
                 read_sensor(i);
                 execute_actuator(i);
             }
@@ -55,132 +62,80 @@ static void tp_execute_task(void *pvParameter){
     }
 }
 
-/*
-  Read values sensed at all available touch pads.
- Print out values in a loop on a serial monitor.
- */
-static void tp_read_task(void *pvParameter)
+static void tp_touch_pad_init(void)
 {
-    uint16_t touch_value_0, touch_value_3, touch_value_4, touch_value_5;
-    uint16_t touch_value_6, touch_value_7, touch_value_8, touch_value_9;
-
-    //printf("Touch Sensor normal mode read, the output format is: \nTouchpad num:[raw data]\n\n");
-    while(1) {
-
-        touch_pad_read(0, &touch_value_0);
-        //touch_pad_read(2, &touch_value_2);
-        touch_pad_read(3, &touch_value_3);
-        touch_pad_read(4, &touch_value_4);
-        touch_pad_read(5, &touch_value_5);
-        touch_pad_read(6, &touch_value_6);
-        touch_pad_read(7, &touch_value_7);
-        touch_pad_read(8, &touch_value_8);
-        touch_pad_read(9, &touch_value_9);
-        
-        //printf("T%d:[%4d] \n", 2, touch_value_2);
-
-        if(touch_value_0 < (uint16_t)100){
-            //printf("\nelectronic injection");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[0] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_3 < (uint16_t)100){
-            //printf("\ninternal temperature");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[1] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_4 < (uint16_t)100){
-            //printf("\nABS");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[2] = 1;
-            car_system_sensors[3] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_5 < (uint16_t)100){
-            //printf("\nairbag");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[4] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_6 < (uint16_t)100){
-            //printf("\nseat belt");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[5] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_7 < (uint16_t)100){
-            //printf("\nfront headlight light");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[6] = 1;
-            car_system_sensors[7] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_8 < (uint16_t)100){
-            //printf("\npower window system");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[8] = 1;
-            car_system_sensors[9] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-
-        if(touch_value_9 < (uint16_t)100){
-            //printf("\ntwo door lock");
-            xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-            car_system_sensors[10] = 1;
-            car_system_sensors[11] = 1;
-            xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
-            //vTaskDelay(2000/portTICK_PERIOD_MS);
-        }
-        
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-
-        //printf("Engine %d\n", car_system_sensors[0]);
+    for (int i = 0;i< TOUCH_PAD_MAX;i++) {
+        //init RTC IO and mode for touch pad.
+        touch_pad_config(i, TOUCH_THRESH_NO_USE);
     }
 }
 
-
-
-static void tp_touch_pad_init(void)
+/*
+  Read values sensed at all available touch pads.
+  Use 2 / 3 of read value as the threshold
+  to trigger interrupt when the pad is touched.
+  Note: this routine demonstrates a simple way
+  to configure activation threshold for the touch pads.
+  Do not touch any pads when this routine
+  is running (on application start).
+ */
+static void tp_example_set_thresholds(void)
 {
-    
-    for (int i = 0;i< TOUCH_PAD_MAX;i++) {
-        touch_pad_config(i, TOUCH_THRESH_NO_USE);
+    uint16_t touch_value;
+    for (int i = 0; i < TOUCH_PAD_MAX; i++) {
+        //read filtered value
+        touch_pad_read_filtered(i, &touch_value);
+        s_pad_init_val[i] = touch_value;
+        ESP_LOGI(TAG, "test init: touch pad [%d] val is %d", i, touch_value);
+        //set interrupt threshold.
+        ESP_ERROR_CHECK(touch_pad_set_thresh(i, touch_value * 2 / 3));
+
     }
-    
+}
+
+/*
+  Handle an interrupt triggered when a pad is touched.
+  Recognize what pad has been touched and save it in a table.
+ */
+static void tp_example_rtc_intr(void *arg)
+{
+    uint32_t pad_intr = touch_pad_get_status();
+    //clear interrupt
+    touch_pad_clear_status();
+    for (int i = 0; i < TOUCH_PAD_MAX; i++) {
+        if ((pad_intr >> i) & 0x01) {
+            s_pad_activated[i] = true;
+        }
+    }
 }
 
 void app_main(void)
 {
+    //uint64_t eus, eus2;
+
+    //eus = esp_timer_get_time();
+    // Initialize touch pad peripheral, it will start a timer to run a filter
+    //ESP_LOGI(TAG, "Initializing touch pad");
     // Initialize touch pad peripheral.
     // The default fsm mode is software trigger mode.
     touch_pad_init();
-
+    // If use interrupt trigger mode, should set touch sensor FSM mode at 'TOUCH_FSM_MODE_TIMER'.
+    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
     // Set reference voltage for charging/discharging
     // In this case, the high reference valtage will be 2.7V - 1V = 1.7V
     // The low reference voltage will be 0.5
     // The larger the range, the larger the pulse count value.
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
     tp_touch_pad_init();
-
+    // Initialize and start a software filter to detect slight change of capacitance.
+    touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
+    // Set thresh hold
+    tp_example_set_thresholds();
+    // Register touch interrupt ISR
+    touch_pad_isr_register(tp_example_rtc_intr, NULL);
     // Create a mutex
     mutex = xSemaphoreCreateMutex();
 
     // Start task to read values sensed by pads
-    xTaskCreate(&tp_read_task, "touch_pad_read_task", 2048, NULL, 5, NULL);
     xTaskCreate(&tp_execute_task, "execute_task", 2048, NULL, 5, NULL);
 }
