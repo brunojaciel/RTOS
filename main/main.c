b6ec7094399ba4dd331488f7ee3f93e096daaf9c
Sérgio Venturi Pereira <sergio.venturi.pereira@gmail.com>
2023-12-03 22:21:27 -0300

    Controle por Threshold

--- main/main.c
+++ main/main.c
@@ -10,13 +10,15 @@
 
 #define TOUCH_THRESH_NO_USE (0)
 #define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
+#define THRESHOLD_OFF 200 // Valor do limiar para desativar
 
 static const char *TAG = "Touch pad";
 
-static bool s_pad_activated[TOUCH_PAD_MAX];
+static volatile bool s_pad_activated[TOUCH_PAD_MAX];
+static uint32_t s_pad_threshold_off[TOUCH_PAD_MAX];
 static uint32_t s_pad_init_val[TOUCH_PAD_MAX];
 
-bool car_system_actuator[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+volatile bool car_system_actuator[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 // 0: electronic injection | 3: internal temperature |
 // 4: ABS | 5: airbag | 6: seat belt |
 // 7: front headlight light | 8: power window system | 9: two door lock
@@ -25,8 +27,13 @@ SemaphoreHandle_t mutex; // Declare a mutex handle
 
 void read_sensor(int index)
 {
+    uint16_t touch_value;
+    touch_pad_read_filtered(index, &touch_value);
     xSemaphoreTake(mutex, portMAX_DELAY); // Take the mutex before accessing the global variable
-    s_pad_activated[index] = false;
+    if (touch_value > s_pad_threshold_off[index])
+    {
+        s_pad_activated[index] = false;
+    }
     xSemaphoreGive(mutex); // Give back the mutex after modifying the global variable
 }
 
@@ -38,7 +45,7 @@ void execute_actuator(int index)
 }
 void colorful_print(const char *name, bool binary)
 {
-    printf("%s: ", name);
+    printf("%s", name);
 
     if (binary == 0)
     {
@@ -58,11 +65,11 @@ static void thread_display(void *pvParameter)
     {
         xSemaphoreTake(mutex, portMAX_DELAY);
         printf("\033[2J\033[1;1H");
-        colorful_print("ENGINE: Actuator electronic injection:", car_system_actuator[0]);
+        colorful_print("ENGINE: Actuator electronic injection: ", car_system_actuator[0]);
         colorful_print("Actuator internal temperature: ", car_system_actuator[3]);
         colorful_print("BRAKE (ABS): ", car_system_actuator[4]);
         colorful_print("Airbag: ", car_system_actuator[5]);
-        colorful_print("Seat belt: ",   car_system_actuator[6]);
+        colorful_print("Seat belt: ", car_system_actuator[6]);
         colorful_print("Front Headlight Light: ", car_system_actuator[7]);
         colorful_print("Power Window System: ", car_system_actuator[8]);
         colorful_print("Two Door Lock: ", car_system_actuator[9]);
@@ -84,10 +91,10 @@ void eletronic_injection(void *pvParameter)
         else
         {
             xSemaphoreTake(mutex, portMAX_DELAY);
-            car_system_actuator[0] = 0;
+            car_system_actuator[0] = false;
             xSemaphoreGive(mutex);
         }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(0.5 / portTICK_PERIOD_MS);
     }
 }
 
@@ -253,6 +260,7 @@ static void tp_example_set_thresholds(void)
         ESP_LOGI(TAG, "test init: touch pad [%d] val is %d", i, touch_value);
         // set interrupt threshold.
         ESP_ERROR_CHECK(touch_pad_set_thresh(i, touch_value * 2 / 3));
+        s_pad_threshold_off[i] = touch_value - 500;
     }
 }
 
@@ -305,14 +313,14 @@ void app_main(void)
 
     // Start tasks to print values sensed by pads
     xTaskCreate(&thread_display, "display_task", 2048, NULL, 5, NULL);
-    
+
     // Start tasks to read values sensed by pads
-    xTaskCreate(&airbag, "airbag_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&abs_brake, "abs_brake_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&seat_belt, "seat_belt_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&two_door_lock, "two_door_lock_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&eletronic_injection, "eletronic_injection_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&power_window_system, "power_window_system_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&airbag, "airbag_task", 2048, NULL, 3, NULL);
+    xTaskCreate(&abs_brake, "abs_brake_task", 2048, NULL, 4, NULL);
+    xTaskCreate(&seat_belt, "seat_belt_task", 2048, NULL, 2, NULL);
+    xTaskCreate(&two_door_lock, "two_door_lock_task", 2048, NULL, 1, NULL);
+    xTaskCreate(&eletronic_injection, "eletronic_injection_task", 2048, NULL, 6, NULL);
+    xTaskCreate(&power_window_system, "power_window_system_task", 2048, NULL, 1, NULL);
     xTaskCreate(&internal_temperature, "internal_temperature_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&front_headlight_light, "front_headlight_light_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&front_headlight_light, "front_headlight_light_task", 2048, NULL, 1, NULL);
 }
